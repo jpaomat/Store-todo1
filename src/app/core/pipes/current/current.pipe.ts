46cd2838a8fc8158415be6d8e155e7d08d4484b6
Jenny Matamoros <jpaomat@gmail.com>
2021-04-03 16:00:57 -0500

    Fixing styles main-products component

--- src/app/core/pipes/current/current.pipe.ts
+++ src/app/core/pipes/current/current.pipe.ts
@@ -6,6 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CurrentPipe implements PipeTransform {
 
   transform(value: any): string {
+    value = (typeof value === 'string') ? parseInt(value) : value;
     return '$' + value.toLocaleString('de-DE', { style: 'decimal' }).toString();
   }
 
